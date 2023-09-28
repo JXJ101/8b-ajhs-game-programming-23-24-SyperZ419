be4fc3b711f60fd092ae5fba077e3ec23092a034
SyperZ419 <s11328481@students.duvalschools.org>
2023-09-28 13:55:01 -0400

    v0.1a
    Declared and defined 3 lists.

--- gameProgramming/computer_science_exercises/02_Collections/collections.py
+++ gameProgramming/computer_science_exercises/02_Collections/collections.py
@@ -1,1 +1,11 @@
-# Collections Examples, Xavier Oliver, v0.0
+# Collections Examples, Xavier Oliver, v0.1a
+
+# LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES
+breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
+# Each item of the list is known as an ELEMENT.
+# The position in the for each is the INDEX.
+# The element "BACON" is at index 0.
+# Python Only: index -1, it is the last item on the list.
+testScores = [95, 100, 25, 15, 27, 35]
+classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
