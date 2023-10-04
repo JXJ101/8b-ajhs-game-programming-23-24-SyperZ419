113e0877d81335128427d1e1b9d127f7e470182f
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-04 15:49:00 -0400

    v0.4
    Added more clarification to the Addition and removal from inventory code.

--- gameProgramming/gaming_exercises/01_collections/collections.py
+++ gameProgramming/gaming_exercises/01_collections/collections.py
@@ -1,13 +1,16 @@
-# Collections Project, Xavier Oliver, v0.2
+# Collections Project, Xavier Oliver, v0.4
 # Adding Items
 playerInventory = []
+inventorySize = 0
 while len(playerInventory) < 10:
-    playerInventory.append(input("Please type an item to add to your inventory, then press ENTER. \n"))
+    playerInventory.append(input(f"You need 10 item(s) in your inventory. \nYou currently have {inventorySize} items in your inventory. \nPlease type an item to add to your inventory, then press ENTER. \n"))
+    inventorySize += 1
 playerInventory.sort()
 print(playerInventory)
 
 # Removing Items
 while len(playerInventory) > 5:
-    playerInventory.remove(input("Please type an item to remove from your inventory, then press ENTER. \n"))
+    playerInventory.remove(input(f"You need to remove 5 items from your inventory. \nYou currently have {inventorySize} items in your inventory. \nPlease type an item to remove from your inventory, then press ENTER. \n"))
 playerInventory.sort()
 print(playerInventory)
+
