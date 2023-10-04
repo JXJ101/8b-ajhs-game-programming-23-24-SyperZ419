9d60324d2a692e179e6f2dc5353a399edb41caa9
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-04 14:47:01 -0400

    v0.2
    Re-did code for adding items

--- gameProgramming/gaming_exercises/01_collections/collections.py
+++ gameProgramming/gaming_exercises/01_collections/collections.py
@@ -1,9 +1,8 @@
 # Collections Project, Xavier Oliver, v0.1
+# Adding Items
 playerInventory = []
-x1 = 0
-while x1 < 10:
+while len(playerInventory) < 10:
     playerInventory.append(input("Please type an item to add to your inventory, then press ENTER. \n"))
-    x1 += 1
 playerInventory.sort()
 print(playerInventory)
 
