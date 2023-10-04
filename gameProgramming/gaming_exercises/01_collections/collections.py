9d9cd98128afd754481eca3523f575b275a73152
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-04 16:08:55 -0400

    v0.5
    Added code for fixed inventory

--- gameProgramming/gaming_exercises/01_collections/collections.py
+++ gameProgramming/gaming_exercises/01_collections/collections.py
@@ -1,16 +1,22 @@
-# Collections Project, Xavier Oliver, v0.4
+# Collections Project, Xavier Oliver, v0.5
 # Adding Items
-playerInventory = []
-inventorySize = 0
-while len(playerInventory) < 10:
-    playerInventory.append(input(f"You need 10 item(s) in your inventory. \nYou currently have {inventorySize} items in your inventory. \nPlease type an item to add to your inventory, then press ENTER. \n"))
-    inventorySize += 1
-playerInventory.sort()
-print(playerInventory)
+# playerInventory = []
+# inventorySize = 0
+# while len(playerInventory) < 10:
+#     playerInventory.append(input(f"You need 10 item(s) in your inventory. \nYou currently have {inventorySize} items in your inventory. \nPlease type an item to add to your inventory, then press ENTER. \n"))
+#     inventorySize += 1
+# playerInventory.sort()
+# print(playerInventory)
 
 # Removing Items
-while len(playerInventory) > 5:
-    playerInventory.remove(input(f"You need to remove 5 items from your inventory. \nYou currently have {inventorySize} items in your inventory. \nPlease type an item to remove from your inventory, then press ENTER. \n"))
-playerInventory.sort()
-print(playerInventory)
+# while len(playerInventory) > 5:
+#     playerInventory.remove(input(f"You need to remove 5 items from your inventory. \nYou currently have {inventorySize} items in your inventory. \nPlease type an item to remove from your inventory, then press ENTER. \n"))
+# playerInventory.sort()
+# print(playerInventory)
+
+# Fixed Inventory System
+weaponList = ["Sword", "Bow", "Axe", "Hammer", "Staff"]
+for x in range(len(weaponList)):
+    print(weaponList [x])
+
 
