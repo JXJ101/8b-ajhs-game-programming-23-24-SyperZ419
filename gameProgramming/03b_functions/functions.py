3eeaaa92211709c5463c94056940c13541f6e2ca
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-23 14:42:18 -0400

    Update functions.py
    Added rollDice

--- gameProgramming/03b_functions/functions.py
+++ gameProgramming/03b_functions/functions.py
@@ -1,3 +1,4 @@
+import random
 # FUNCTION -- a named piece of code that can be reused easily. 
 # FUNCTION SIGNATURE -- Syntax for creating a new function. 
 def exampleFunctionA(): # NO PARAMETERS
@@ -15,5 +16,19 @@ def exampleFunctionB(num, count): # PARAMETERS
 # IF A FUNCTION REQUIRES PARAMETERS, YOUR CODE WILL CRASH WITHOUT THEM!
 
 # RUNNING A FUCTION IS KNOWN AS CALLING THE FUNCTION
-exampleFunctionA()
-exampleFunctionB(5, 0)
+# exampleFunctionA()
+# exampleFunctionB(5, 0)
+
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        print(f"Roll: {roll}\n")
+        numRolled += 1
+
+rollDice(4, 12)
+rollDice(3, 20)
+
+
+
+
