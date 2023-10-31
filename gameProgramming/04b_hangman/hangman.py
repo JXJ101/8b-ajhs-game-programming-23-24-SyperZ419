94a6dd8cea1a66ffc011e5bf0d4abf153388b7c7
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-31 14:15:15 -0400

    Update hangman.py
    added HANGMAN_BOARD list

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,4 +1,43 @@
-# Hangman Game By Xavier Oliver, v0.0
+# Hangman Game By Xavier Oliver, v0.2
 
 words = 'cat heat fold paper freeze keys light gate ham corn fiend utopia twilight sporange potato dystopia falcon raptor fossilized permeate pragmatic cornucopia pneumonoultramicroscopicsilicovolcanoconiosis hippopotomonstrosesquippedaliophobia Honorificabilitudinitatibus Floccinaucinihilipilification incomprehensible sigma gamma agglutinative variable volcanic metamorphic'.split()
-print(words)
+
+# VARIABLE NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = ['''
+    +---+
+        |
+        |
+        |
+     =======''', '''   
+    +---+
+    O   |
+        |
+        |
+     =======''', '''
+    +---+
+    O   |
+    |   |
+        |
+     =======''', '''
+    +---+
+    O   |
+   /|   |
+        |
+     =======''', '''
+    +---+
+    O   |
+   /|\  |
+        |
+     =======''', '''            
+    +---+
+    O   |
+   /|\  |
+   /    |
+     =======''', '''           
+    +---+
+    O   |
+   /|\  |
+   / \  |
+     =======''']
+                   
+     
