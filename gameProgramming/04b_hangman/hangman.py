2cfcba348d8870caa8d7ad18c358342cda61b0bb
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-31 13:58:13 -0400

    Update hangman.py
    added words list with .split

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,1 +1,4 @@
 # Hangman Game By Xavier Oliver, v0.0
+
+words = 'cat heat fold paper freeze keys light gate ham corn fiend utopia twilight sporange potato dystopia falcon raptor fossilized permeate pragmatic cornucopia pneumonoultramicroscopicsilicovolcanoconiosis hippopotomonstrosesquippedaliophobia Honorificabilitudinitatibus Floccinaucinihilipilification incomprehensible sigma gamma agglutinative variable volcanic metamorphic'.split()
+print(words)
