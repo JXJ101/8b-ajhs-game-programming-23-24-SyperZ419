756e76a1c7a5b04acb7c3a4959b12fba67ff9a6d
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-31 14:34:48 -0400

    Update hangman.py
    added function to generate random word

--- gameProgramming/04b_hangman/hangman.py
+++ gameProgramming/04b_hangman/hangman.py
@@ -1,6 +1,6 @@
-# Hangman Game By Xavier Oliver, v0.2
-
-words = 'cat heat fold paper freeze keys light gate ham corn fiend utopia twilight sporange potato dystopia falcon raptor fossilized permeate pragmatic cornucopia pneumonoultramicroscopicsilicovolcanoconiosis hippopotomonstrosesquippedaliophobia Honorificabilitudinitatibus Floccinaucinihilipilification incomprehensible sigma gamma agglutinative variable volcanic metamorphic'.split()
+# Hangman Game By Xavier Oliver, v0.3
+import random
+words = 'cat heat fold paper freeze keys light gate ham corn fiend utopia twilight sporange potato dystopia falcon raptor fossilized permeate pragmatic cornucopia pneumonoultramicroscopicsilicovolcanoconiosis hippopotomonstrosesquippedaliophobia honorificabilitudinitatibus floccinaucinihilipilification incomprehensible sigma mitochondrion agglutinative variable volcanic metamorphic'.split()
 
 # VARIABLE NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
 HANGMAN_BOARD = ['''
@@ -40,4 +40,14 @@ HANGMAN_BOARD = ['''
    / \  |
      =======''']
                    
-     
+# Pick Word from List
+def getRandomWord(wordList): # Return a random word from the list.
+    wordIndex = random.randint(0, len(wordList) - 1 )
+    # len(listname) - 1 is  EXTREMELY COMMON WHEN WORKING WITH LISTS.
+    return wordList[wordIndex]
+
+i = 0
+while i < 50 :
+    word = getRandomWord(words)
+    print(word)
+    i += 1
