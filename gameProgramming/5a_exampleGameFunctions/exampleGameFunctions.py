ec50cfef7fe586b7e3dd2fa03aa1cb87a00d25b7
SyperZ419 <s11328481@students.duvalschools.org>
2023-11-13 13:51:13 -0500

    Update exampleGameFunctions.py
    added 4 template functions

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,1 +1,13 @@
 # Example Game Functions Project, Xavier Oliver, v0.0
+import random
+def functionOne():
+    pass
+
+def functionTwo(param1):
+    pass
+
+def functionThree(param1 = "Default Value"):
+    pass
+
+def functionFour(param1, param2, param3):
+    pass
