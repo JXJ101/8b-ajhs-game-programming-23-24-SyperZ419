50ff8c66abe0d206d656661576cf0871b55c4179
SyperZ419 <s11328481@students.duvalschools.org>
2023-11-19 18:29:33 -0500

    Update exampleGameFunctions.py
    finished gameOver function

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -65,7 +65,8 @@ def playerName(): # Obtains character's name from user input. No parameters. Ret
 #     return skill
 
 def gameOver(playerCurrentHealth = playerHealth): # Checks to see if the player's health is less than or equal to zero, then tells them the game has ended. Requires player's current health. No return.
-    pass
+    if playerCurrentHealth <= 0:
+        input('You have lost all of your health. \nGame Over.')
 
 
 
