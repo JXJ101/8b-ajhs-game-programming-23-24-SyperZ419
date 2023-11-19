194f0d7beb4ae6bc5592456ca50daf3ad480eb63
SyperZ419 <s11328481@students.duvalschools.org>
2023-11-19 17:32:54 -0500

    Update exampleGameFunctions.py
    tweaked playerName function
    
    scrapped playerTurn (formerly playerHealthCheck) for now

--- gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming/5a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,6 +1,6 @@
 # Example Game Functions Project, Xavier Oliver, v0.3
 import random
-movelist = ['Fireball', 'Ice Beam', 'Thunderbolt','Sword Slash', 'Kamikaze', 'Heal', 'Guard']
+skillList = ['fireball', 'icebeam', 'thunderbolt','sword slash', 'kamikaze', 'heal', 'guard']
 enemyList = ['Goblin', 'Goblin Soldier', 'Goblin Chief', 'Skeleton Knight', 'Haunted Armor', 'Undead Wizard', 'Dragon', 'Hydra']
 playerHealth = 150
 def generatePlayerStats(): # Generates player stats. No parameters required. Returns the stats list.
@@ -19,7 +19,7 @@ def damageCalc(skillUsed, enemyAttackValue, defense = statsList[1]): # Calculate
     if damageTaken <= 0:
         damageTaken = 0
         print('You took no damage.')
-    elif skillUsed == 'Guard':
+    elif skillUsed == 'guard':
         guardActive = True
         if guardActive == True:
             damageTaken /= 2
@@ -29,7 +29,7 @@ def damageCalc(skillUsed, enemyAttackValue, defense = statsList[1]): # Calculate
             print(f'Since guard was active, you took {damageTaken} damage.')
     else:
         print(f'You took {damageTaken} damage.')
-    return damageTaken
+    return damageTaken # Returns player's health.
     
 def enemySelect(): # Chooses a random enemy and amount of enemies. No parameters required.
     i = random.randint(0, len(enemyList) - 1)
@@ -43,18 +43,32 @@ def enemySelect(): # Chooses a random enemy and amount of enemies. No parameters
         
 def playerName(): # Obtains character's name from user input. No parameters. Returns name of character.
     name = input('Name your character.')
-    nameConfirm = input(f'Your input name is {name}, correct? \nPlease input either yes or no.').lower
-    if nameConfirm == 'yes':
+    nameConfirm = int(input(f'Your input name is {name}, correct? \nPlease input either yes(1) or no(2).'))
+    if nameConfirm == 1:
         print('Character name confirmed.')
     else:
-        nameConfirm = input('Please re-input the name of your character.').lower
+        nameConfirm = input('Please re-input the name of your character.')
         print('Character name confirmed.')
     return name
 
-def playerHealthCheck(playerCurrentHealth = playerHealth):
-    pass
+
+# def playerTurn(playerCurrentHealth = playerHealth): # Determines if the player can make their turn, then sees what skill they use. Requires player health. Returns skill used.
+#     while playerHealth > 0:
+#         print(skillList)
+#         skill = input('Please select an action from your skill list.').lower
+#         if skill in skillList == True:
+#             print(f'You used {skill}.')
+#             break
+#         else:
+#             skill = input('Chosen skill not found. Please choose a skill from your skill list.').lower
+#             break
+#     return skill
+
+
+
 
 playerName()
 enemySelect()
-damageCalc('Guard', 33.7)
-
+skillUsed = playerTurn()
+playerDamage = damageCalc(skillUsed, 45)
+playerHealth -= playerDamage
